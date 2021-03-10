b74815c0c7ea81e3d9b09f4cef2d8c0a8f82e7ca
askador <fedosov0912@gmail.com>
2021-03-10 13:17:08 +0200

    configs for databases

--- data/config.py
+++ data/config.py
@@ -1,11 +1,21 @@
-token = '1482334694:AAH6GzEuYH34ZOwuoXgZttO87lcP9WiH_B8'
+# token = '1683570768:AAFydK1ItIvQc2soe2LLAqxw9Q1XjcwihDE'
 
-db_setting = {
+token = '1173133322:AAG_E7H2IjRypO3dt-pygUjh9V1HP8X8JPk' # test_bot
+
+postgresql_db_setting = {
     "Host": "ec2-34-224-229-81.compute-1.amazonaws.com",
     "Database": "d7kotnui7ubmvq",
     "User": "rjblaxrzxaxpum",
     "Port": "5432",
     "Password": "ea00c427c5ef18079f94af132a47f42fe10ddc81393bdd48ba2c84b708f1a1d1"
+}
+
+mongodb_setting = {
+    "User": "test_user",
+    "Password": "1234",
+    "Host": "cluster0.lajfk.mongodb.net",
+    "Database": "hw_bot_db",
+    "args": "retryWrites=true&w=majority"
 }
 
 admins = [
@@ -20,4 +30,8 @@ commands = {
     '/help': "",
 }
 
-db_url = f"postgres://{db_setting['User']}:{db_setting['Password']}@{db_setting['Host']}:{db_setting['Port']}/{db_setting['Database']}"
+postresql_db_url = f"postgres://{postgresql_db_setting['User']}:{postgresql_db_setting['Password']}" \
+         f"@{postgresql_db_setting['Host']}:{postgresql_db_setting['Port']}/{postgresql_db_setting['Database']}"
+
+mongodb_url = f"mongodb+srv://{mongodb_setting['User']}:{mongodb_setting['Password']}@" \
+              f"{mongodb_setting['Host']}/{mongodb_setting['Database']}?{mongodb_setting['args']}"
