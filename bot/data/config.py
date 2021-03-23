d96b9bb541236ee5030b48ae96f61557a22af20e
askador <fedosov0912@gmail.com>
2021-03-23 17:32:29 +0200

    some changes

--- bot/data/config.py
+++ bot/data/config.py
@@ -2,11 +2,19 @@
 token = '1482334694:AAH6GzEuYH34ZOwuoXgZttO87lcP9WiH_B8' #shishki
 test_bot_token = '1173133322:AAG_E7H2IjRypO3dt-pygUjh9V1HP8X8JPk' # test_bot
 
+postgresql_db_setting = {
+    "Host": "ec2-34-224-229-81.compute-1.amazonaws.com",
+    "Database": "d7kotnui7ubmvq",
+    "User": "rjblaxrzxaxpum",
+    "Port": "5432",
+    "Password": "ea00c427c5ef18079f94af132a47f42fe10ddc81393bdd48ba2c84b708f1a1d1"
+}
+
 mongodb_setting = {
     "User": "test_user",
     "Password": "1234",
     "Host": "cluster0.lajfk.mongodb.net",
-    "Database": "aiogram_fsm",
+    "Database": "hw_bot_db",
     "args": "retryWrites=true&w=majority"
 }
 
@@ -38,10 +46,10 @@ commands = {
 
 }
 
-mongodb_url = f"mongodb+srv://{mongodb_setting['User']}:{mongodb_setting['Password']}@" \
-             f"{mongodb_setting['Host']}/{mongodb_setting['Database']}?{mongodb_setting['args']}"
+#mongodb_url = f"mongodb+srv://{mongodb_setting['User']}:{mongodb_setting['Password']}@" \
+#              f"{mongodb_setting['Host']}/{mongodb_setting['Database']}?{mongodb_setting['args']}"
 
-# mongodb_url = f"mongodb+srv://{mongodb_setting1['User']}:{mongodb_setting1['Password']}@" \
-#               f"{mongodb_setting1['Host']}/{mongodb_setting1['Database']}?{mongodb_setting1['args']}"
+mongodb_url = f"mongodb+srv://{mongodb_setting1['User']}:{mongodb_setting1['Password']}@" \
+              f"{mongodb_setting1['Host']}/{mongodb_setting1['Database']}?{mongodb_setting1['args']}"
 
 #mongodb_url = "mongodb+srv://test_user:1234@cluster0.lajfk.mongodb.net/aiogram_fsm?retryWrites=true&w=majority"
