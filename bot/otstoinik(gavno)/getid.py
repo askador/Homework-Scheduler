db2a38eeb869d14fd52c0588e48321bcd587dba2
Alexiush <streltsov2609@gmail.com>
2021-04-06 15:38:54 +0300

    нахуй отстойник

--- bot/otstoinik(gavno)/getid.py
+++
@@ -1,9 +0,0 @@
-from bot.loader import dp
-
-
-@dp.message_handler(lambda msg: msg.reply_to_message is not None and msg.text.lower() == "getid")
-async def get_id(msg):
-    try:
-        await msg.reply(msg.reply_to_message.from_user.id)
-    except Exception:
-        pass
