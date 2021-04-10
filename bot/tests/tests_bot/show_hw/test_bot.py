076446e989804e770edc7dd590721bc74b3e3014
askador <fedosov0912@gmail.com>
2021-04-10 12:04:51 +0300

    test bot

--- bot/tests/tests_bot/show_hw/test_bot.py
+++ bot/tests/tests_bot/show_hw/test_bot.py
@@ -1,37 +1,29 @@
 import logging
-import os
-from datetime import datetime, timedelta
-from pprint import pprint
 
-from aiogram.types import ContentType
-from pymongo import MongoClient
 from aiogram.utils import executor
-from aiogram.dispatcher.filters import Text, Command
+from aiogram.dispatcher.filters import Text
 from bot.loader import bot, dp
-# from bot.handlers.get_hw_public import get_hw_public
-from bot.types.MongoDB.Collections import Chat
+
+pinned = []
 
 
-@dp.message_handler(Text(equals="чек"), user_id=526497876, state='*')
+@dp.message_handler(Text(equals="пин"), user_id=526497876, state='*')
 async def add_chat(msg):
     chat_id = -1001424619068
-    ch = Chat(chat_id)
-    s = await ch.get_field_value('subjects')
-    print(type(s))
 
-    #
-    # col.update_one({"_id": -1001424619068}, {"$push": {"homeworks":
-    #     {
-    #         '_id': 11,
-    #         'subject': 'чм',
-    #         'subgroup': '',
-    #         'name': 'лаба',
-    #         'description': 'описание описание описание описание описание описание описание',
-    #         'deadline': datetime(2021, 3, 15, 0, 0),
-    #         'priority': 0
-    #     },
-    # }})
+    pinned.append(msg.message_id)
 
+    a = await bot.pin_chat_message(chat_id, msg.message_id)
+    print(a)
+
+
+@dp.message_handler(Text(equals="-пин"), user_id=526497876, state='*')
+async def add_chat(msg):
+    global pinned
+    chat_id = -1001424619068
+    a = await bot.unpin_chat_message(chat_id, pinned[-1]+3)
+    print(a)
+    # del pinned[-1]
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
