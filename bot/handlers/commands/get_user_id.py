16f931e2bde1216d95034d0aec489bb72734a636
askador <fedosov0912@gmail.com>
2021-04-17 13:38:06 +0300

    get user id

--- bot/handlers/commands/get_user_id.py
+++ bot/handlers/commands/get_user_id.py
@@ -2,8 +2,8 @@ from bot.loader import dp
 from aiogram.dispatcher import filters
 
 
-@dp.message_handler(filters.IsReplyFilter(is_reply=True), filters.Text(equals="getid"), state='*')
-async def get_id(msg):
+@dp.message_handler(filters.IsReplyFilter(is_reply=True), filters.Text(equals="get id"), state='*')
+async def get_user_id(msg):
     try:
         await msg.reply(msg.reply_to_message.from_user.id)
     except Exception:
