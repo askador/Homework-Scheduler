b8fa3870581e9dcadad4becba24c05fa8ab926e2
askador <fedosov0912@gmail.com>
2021-04-19 10:21:33 +0300

    max chat amount

--- bot/__main__.py
+++ bot/__main__.py
@@ -16,7 +16,7 @@ async def on_startup(dp: Dispatcher):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    dp.middleware.setup(middlewares.ChatAmount)
+    dp.middleware.setup(middlewares.ChatAmount())
     executor.start_polling(
         dp, on_startup=on_startup, skip_updates=True
     )
