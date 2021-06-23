985df5840b5abbb30ce9da381f8d5406e2b3a1f2
askador <53081549+askador@users.noreply.github.com>
2021-06-23 15:09:53 +0300

    Update loader.py

--- bot/loader.py
+++ bot/loader.py
@@ -8,7 +8,7 @@ bot = Bot(
     parse_mode=types.ParseMode.HTML,
 )
 
-storage = MongoStorage(uri=config.mongodb_url.replace("hw_bot_db", "aiogram_fsm"))
+storage = MongoStorage(uri=config.mongodb_url.replace("db_name", "aiogram_fsm"))
 # storage = MongoStorage(uri="mongodb+srv://test_user:1234@cluster0.lajfk.mongodb.net/aiogram_fsm?retryWrites=true&w=majority")
 
 dp = Dispatcher(
